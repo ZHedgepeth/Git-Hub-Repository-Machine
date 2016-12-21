e14f49e5336c1914bcdcc671643019fc709b94f2
zach <zhedgepeth1124@gmail.com>
2016-12-21 16:30:47 -0500

    fixed spacing in backend file on lines 20-22 and placed your api key goes here

--- js/repository-search.js
+++ js/repository-search.js
@@ -11,15 +11,18 @@ GitHub.prototype.getRepos = function(username, displayFunction) {
     $('#showRepositories').text(error.responseJSON.message);
   });
 
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response)
+  {
     console.log(response);
   $("#showRepositories").text("");
-  for(var index=0; index<response.length;index++){
-    $("#showRepositories").append("<li class='repoName'> REPOSITORY: " + response[index].name + "<ul><li class='repoDate'> DATE CREATED: " + response[index].created_at +  "<li class='repoDesc'>" + response[index].description + "</li></ul>" + "</li>")
-  }
-}).fail(function(error){
-  console.log(error.responseJSON.message);
-});
+      for(var index=0; index<response.length;index++)
+      {
+        $("#showRepositories").append("<li class='repoName'> REPOSITORY: " + response[index].name + "<ul><li class='repoDate'> DATE CREATED: " + response[index].created_at +  "<li class='repoDesc'>" + response[index].description + "</li></ul>" + "</li>")
+      }
+  }).fail(function(error)
+  {
+    console.log(error.responseJSON.message);
+  });
 };
 
 exports.gitHubModule = GitHub;
